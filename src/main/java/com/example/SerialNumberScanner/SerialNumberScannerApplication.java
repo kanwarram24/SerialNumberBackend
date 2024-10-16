d6733f707342512136646a6b6cf037fb972a6e76
Kanwar Ram <kanwarram17@gmail.com>
2024-10-16 11:18:48 +1100

    Intial Package

---
+++ src/main/java/com/example/SerialNumberScanner/SerialNumberScannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SerialNumberScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerialNumberScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerialNumberScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SerialNumberScanner/SerialNumberScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SerialNumberScanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerialNumberScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
